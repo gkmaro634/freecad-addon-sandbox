6fc6d39a2cc7e1534fd7e0860c3206eaaab16f1a
Kazuma Goto <kazuma.gotoh@jp.ricoh.com>
2023-12-01 10:15:23 +0900

    get_module_pathのコメントアウトを解除

--- SandboxWorkbench.py
+++ SandboxWorkbench.py
@@ -11,13 +11,13 @@ from FreeCAD import Vector, Rotation, activeDocument
 # def recompute():
 #     activeDocument().recompute()
 
-# def get_module_path():
-#     ''' Returns the current module path.
-#     Determines where this file is running from, so works regardless of whether
-#     the module is installed in the app's module directory or the user's app data folder.
-#     (The second overrides the first.)
-#     '''
-#     return os.path.dirname(__file__)
+def get_module_path():
+    ''' Returns the current module path.
+    Determines where this file is running from, so works regardless of whether
+    the module is installed in the app's module directory or the user's app data folder.
+    (The second overrides the first.)
+    '''
+    return os.path.dirname(__file__)
 
 
 # def makeRay(position = Vector(0, 0, 0),
